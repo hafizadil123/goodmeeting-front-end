1c38106133da4063a0213892465ac5cb661f2dcf
Adil Sikander <adilsikander@ml-mbps-mbp.home>
2020-01-05 23:46:08 +0500

    new build

--- app/utils/constants.js
+++ app/utils/constants.js
@@ -2,4 +2,5 @@ export const RESTART_ON_REMOUNT = '@@saga-injector/restart-on-remount';
 export const DAEMON = '@@saga-injector/daemon';
 export const ONCE_TILL_UNMOUNT = '@@saga-injector/once-till-unmount';
 export const BASE_URL = 'http://18.224.1.173/';
+
 export const MOCK_URL = 'http://www.mocky.io/v2/5df8edca300000f10988a092';
