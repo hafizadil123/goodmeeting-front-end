9fc79956012d42150f10fa18ab1dd9c7f2694f8a
Adil Sikander <adilsikander@ml-mbps-mbp.home>
2020-01-20 01:17:44 +0500

    thanks you message logo

--- app/components/Dashboard/ThankyouMessage/index.js
+++ app/components/Dashboard/ThankyouMessage/index.js
@@ -12,7 +12,9 @@ const ThankYouMessage = () => (
         <div className="container">
           <div className="row">
             <div className="col">
-              <div className="logo">{/* <img src={logo} className /> */}</div>
+              <div className="logo">
+                <img src={logo} className />
+              </div>
             </div>
             <div className="col">
               <Link to="#" className="btn btn-dark btn-md btn-black">
@@ -26,7 +28,7 @@ const ThankYouMessage = () => (
         <div className="container">
           <div className="row">
             <div className="col-md-12">
-              <img src={hsbclogo} className="hsbclogo" />
+              {/* <img src={hsbclogo} className="hsbclogo" /> */}
               <h1 className="heading1">Thank You for your feedback</h1>
               <p>We will improve and hope to have GoodMeetings Everyday.</p>
               <a href="#" className="btn btn-dark btn-md btn-black">
