da906ec3b2154e1fc24bd5af1000b458f14f90c2
Adil Sikander <adilsikander@ml-mbps-mbp.home>
2020-01-30 02:20:04 +0500

    live url

--- app/utils/constants.js
+++ app/utils/constants.js
@@ -1,6 +1,6 @@
 export const RESTART_ON_REMOUNT = '@@saga-injector/restart-on-remount';
 export const DAEMON = '@@saga-injector/daemon';
 export const ONCE_TILL_UNMOUNT = '@@saga-injector/once-till-unmount';
-export const BASE_URL = 'http://13.59.166.99/';
+export const BASE_URL = 'https://api.goodmeeting.today/';
 
 export const MOCK_URL = 'http://www.mocky.io/v2/5df8edca300000f10988a092';
