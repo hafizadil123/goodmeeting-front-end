d5c6ed4bf803b9e9d42ec1c5b5e08a54fe910401
waqasahmad34 <internetfaizan@gmail.com>
2020-12-11 19:42:19 +0200

    favicon and range slider style

--- babel.config.js
+++ babel.config.js
@@ -12,7 +12,9 @@ module.exports = {
     'styled-components',
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-syntax-dynamic-import',
+
   ],
+  
   env: {
     production: {
       only: ['app'],
