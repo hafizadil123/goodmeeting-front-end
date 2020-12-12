ca928ff258239da12868d10e8a5588e4a279cd78
waqasahmad34 <48016736+waqasahmad34@users.noreply.github.com>
2020-12-12 21:01:10 +0500

    favicon code merge

--- app/components/Dashboard/Feedback1/index.js
+++ app/components/Dashboard/Feedback1/index.js
@@ -14,11 +14,8 @@ import Slider from 'react-rangeslider';
 import { BASE_URL } from '../../../utils/constants';
 import { createFeedbacks } from '../../../utils/requests';
 import logoImage from '../../../assets/images/logo.png';
-
 // To include the default styles
 import 'react-rangeslider/lib/index.css';
-
-
 
 const FeedbackForm = ({ history }) => {
   const [page, setPage] = useState(0);
